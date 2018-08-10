14810cc2ed085f89592ae02dfa9bef7ba26352d3
ananyagupta1114 <ananyagupta1114@gmail.com>
2018-08-10 16:25:01 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,49 @@
 # book.rb
 # Add you Book class here
+class Book
+  attr_accessor :author, :genre, :page_count #setters and gettters for each of these attributes
+  attr_reader :title 
+  
+  def initialize(title)
+    @title = title
+  end
+  # def title #getter method
+  #   @title
+  # end
+  
+  # def author=(author) #setter method
+  #   @author=author 
+  # end
+  
+  # def author #getter method
+  #   @author
+  # end
+  
+  # def page_count=(page_count) #setter method
+  #   @page_count=page_count
+  # end
+  
+  # def page_count #getter method
+  #   @page_count
+  # end
+  
+  # def genre=(genre) #setter method
+  #   @genre = genre
+  # end
+  
+  # def genre #getter method 
+  #   @genre
+  # end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+  def love
+    puts "I love the author #{author}."
+  
+end
+
+wonder = Book.new("Wonder")
+wonder.author = "RJ Palacio"
+wonder.love
+end
